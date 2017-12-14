9d220e7358c3f4d8081e7b12726890d909618bd0
TJ Taylor <dugancathal@gmail.com>
2017-12-13 21:37:38 -0700

    Shamelessly green - first pass

--- src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
+++ src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
@@ -1,7 +1,28 @@
 package com.tjtjrb.bottles
 
 class Bottles {
-    fun verse(i: Int): String {
-        return ""
+    fun verse(number: Int): String {
+        return when (number) {
+            2 -> """
+                2 bottles of beer on the wall, 2 bottles of beer.
+                Take one down and pass it around, 1 bottle of beer on the wall.
+            """.trimIndent()
+            1 -> """
+                1 bottle of beer on the wall, 1 bottle of beer.
+                Take it down and pass it around, no more bottles of beer on the wall.
+            """.trimIndent()
+            0 -> """
+                No more bottles of beer on the wall, no more bottles of beer.
+                Go to the store and buy some more, 99 bottles of beer on the wall.
+            """.trimIndent()
+            else -> """
+                $number bottles of beer on the wall, $number bottles of beer.
+                Take one down and pass it around, ${number - 1} bottles of beer on the wall.
+            """.trimIndent()
+        }
+    }
+
+    fun verses(start: Int, end: Int): String? {
+        return start.downTo(end).joinToString("\n") { verse(it) }
     }
 }
