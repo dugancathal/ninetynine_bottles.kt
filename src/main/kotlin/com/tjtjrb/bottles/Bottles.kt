a506751b3c91db2ba66e4cc5120348d045ad3565
TJ Taylor <dugancathal@gmail.com>
2017-12-13 22:09:41 -0700

    Extract BottleNumber class.

--- src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
+++ src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
@@ -6,43 +6,47 @@ class Bottles {
     }
 
     fun verse(number: Int): String {
+        val bottleNumber = BottleNumber(number)
+        val nextBottleNumber = BottleNumber(BottleNumber(number).next())
         return """
-            ${quantity(number).capitalize()} ${container(number)} of beer on the wall,
-            ${quantity(number)} ${container(number)} of beer.
-            ${action(number)},
-            ${quantity(next(number))} ${container(next(number))} of beer on the wall.
+            ${bottleNumber.quantity().capitalize()} ${bottleNumber.container()} of beer on the wall,
+            ${bottleNumber.quantity()} ${bottleNumber.container()} of beer.
+            ${bottleNumber.action()},
+            ${nextBottleNumber.quantity()} ${nextBottleNumber.container()} of beer on the wall.
         """.trimIndent()
     }
+}
 
-    private fun action(number: Int): String {
+class BottleNumber(private val number: Int) {
+    fun action(): String {
         return if (number == 0)
             "Go to the store and buy some more"
         else
-            "Take ${pronoun(number)} down and pass it around"
+            "Take ${pronoun()} down and pass it around"
     }
 
-    private fun next(number: Int): Int {
+    fun next(): Int {
         return if (number == 0)
             99
         else
             number - 1
     }
 
-    private fun container(number: Int): String {
+    fun container(): String {
         return if (number == 1)
             "bottle"
         else
             "bottles"
     }
 
-    private fun pronoun(number: Int): String {
+    fun pronoun(): String {
         return if (number == 1)
             "it"
         else
             "one"
     }
 
-    private fun quantity(number: Int): String {
+    fun quantity(): String {
         return if (number == 0)
             "no more"
         else
