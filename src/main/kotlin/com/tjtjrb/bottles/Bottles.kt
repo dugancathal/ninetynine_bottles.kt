eb94a358f8b0ab28803a30583ec37fa84bcb21e1
TJ Taylor <dugancathal@gmail.com>
2017-12-13 22:29:00 -0700

    Fix LSP issue. Add toString()

--- src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
+++ src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
@@ -7,40 +7,37 @@ class Bottles {
 
     fun verse(number: Int): String {
         val bottleNumber = BottleNumber.forNum(number)
-        val nextBottleNumber = BottleNumber.forNum(BottleNumber.forNum(number).next())
-        return """
-            ${bottleNumber.quantity().capitalize()} ${bottleNumber.container()} of beer on the wall,
-            ${bottleNumber.quantity()} ${bottleNumber.container()} of beer.
-            ${bottleNumber.action()},
-            ${nextBottleNumber.quantity()} ${nextBottleNumber.container()} of beer on the wall.
-        """.trimIndent()
+        return "$bottleNumber of beer on the wall,\n".capitalize() +
+            "$bottleNumber of beer.\n" +
+            "${bottleNumber.action()},\n" +
+            "${bottleNumber.next()} of beer on the wall."
     }
 }
 
 open class BottleNumber(private val number: Int) {
     companion object {
         fun forNum(number: Int): BottleNumber {
-            return if (number == 0) {
-                BottleNumber0()
-            } else if (number == 1) {
-                BottleNumber1()
-            } else {
-                BottleNumber(number)
+            return when (number) {
+                0 -> BottleNumber0()
+                1 -> BottleNumber1()
+                else -> BottleNumber(number)
             }
         }
     }
 
     open fun action(): String = "Take ${pronoun()} down and pass it around"
     open fun quantity(): String = "$number"
-    open fun next(): Int = number - 1
+    open fun next(): BottleNumber = BottleNumber.forNum(number - 1)
 
     open fun container(): String = "bottles"
     open fun pronoun(): String = "one"
+
+    override fun toString(): String = "${quantity()} ${container()}"
 }
 
 class BottleNumber0 : BottleNumber(0) {
     override fun action(): String = "Go to the store and buy some more"
-    override fun next(): Int = 99
+    override fun next(): BottleNumber = BottleNumber.forNum(99)
     override fun quantity(): String = "no more"
 }
 
