bac13bff3e25d809e1536d2baecedd38f3d63fa2
TJ Taylor <dugancathal@gmail.com>
2017-12-13 21:45:15 -0700

    Refactor out the container

--- src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
+++ src/main/kotlin/com/tjtjrb/bottles/Bottles.kt
@@ -3,26 +3,29 @@ package com.tjtjrb.bottles
 class Bottles {
     fun verse(number: Int): String {
         return when (number) {
-            2 -> """
-                2 bottles of beer on the wall, 2 bottles of beer.
-                Take one down and pass it around, 1 bottle of beer on the wall.
+            0 -> """
+                No more bottles of beer on the wall, no more bottles of beer.
+                Go to the store and buy some more, 99 bottles of beer on the wall.
             """.trimIndent()
             1 -> """
                 1 bottle of beer on the wall, 1 bottle of beer.
                 Take it down and pass it around, no more bottles of beer on the wall.
             """.trimIndent()
-            0 -> """
-                No more bottles of beer on the wall, no more bottles of beer.
-                Go to the store and buy some more, 99 bottles of beer on the wall.
-            """.trimIndent()
             else -> """
                 $number bottles of beer on the wall, $number bottles of beer.
-                Take one down and pass it around, ${number - 1} bottles of beer on the wall.
+                Take one down and pass it around, ${number - 1} ${container(number - 1)} of beer on the wall.
             """.trimIndent()
         }
     }
 
     fun verses(start: Int, end: Int): String? {
         return start.downTo(end).joinToString("\n") { verse(it) }
+    }
+
+    private fun container(number: Int): String {
+        return if (number == 1)
+            "bottle"
+        else
+            "bottles"
     }
 }
